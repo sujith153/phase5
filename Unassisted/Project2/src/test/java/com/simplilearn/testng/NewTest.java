411e37aed4196e83234c42e330188793d57e9db8
sujith <sujithvardhanreddy153@gmail.com>
2022-10-04 22:15:09 +0530

    Testng

---
+++ Unassisted/Project2/src/test/java/com/simplilearn/testng/NewTest.java
@@ -0,0 +1,101 @@
+package com.simplilearn.testng;
+
+import org.testng.annotations.Test;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class NewTest {
+	WebDriver driver;
+	@Test
+	public void oracle() throws InterruptedException {
+		
+		driver.get("https://profile.oracle.com/myprofile/account/create-account.jspx");
+		driver.manage().window().maximize();
+		Thread.sleep(1000);
+		
+		
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		WebElement email = driver.findElement(By.cssSelector("#sView1\\:r1\\:0\\:email\\:\\:content"));
+		email.sendKeys("sujith@gmail.com");
+		Thread.sleep(1000);
+		
+		
+		
+		WebElement password = driver.findElement(By.cssSelector("#sView1\\:r1\\:0\\:password\\:\\:content"));
+		password.sendKeys("Reddy@15353");
+		
+		Thread.sleep(1000);
+		
+		WebElement repassword = driver.findElement(By.cssSelector("#sView1\\:r1\\:0\\:retypePassword\\:\\:content"));
+		repassword.sendKeys("Reddy@15353");
+		
+		driver.manage().timeouts().setScriptTimeout(10, TimeUnit.SECONDS);
+		
+		WebElement country = driver.findElement(By.cssSelector("#sView1\\:r1\\:0\\:country\\:\\:content"));
+		country.sendKeys("India");
+		driver.manage().timeouts().setScriptTimeout(10, TimeUnit.SECONDS);
+		
+		WebElement firstname = driver.findElement(By.cssSelector("#sView1\\:r1\\:0\\:firstName\\:\\:content"));
+		firstname.sendKeys("chennupalli");
+		
+		driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+		
+		WebElement lastname = driver.findElement(By.cssSelector("#sView1\\:r1\\:0\\:lastName\\:\\:content"));
+		lastname.sendKeys("sujith vardhan reddy");
+		
+		driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+		
+		WebElement jobTitle = driver.findElement(By.cssSelector("#sView1\\:r1\\:0\\:jobTitle\\:\\:content"));
+		jobTitle.sendKeys("Engineer");
+		
+		driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+		
+		WebElement workPhone = driver.findElement(By.cssSelector("#sView1\\:r1\\:0\\:workPhone\\:\\:content"));
+		workPhone.sendKeys("8793876751");
+		Thread.sleep(1000);
+		
+		WebElement companyName = driver.findElement(By.cssSelector("#sView1\\:r1\\:0\\:companyName\\:\\:content"));
+		companyName.sendKeys("cherry Pvt Ltd");
+		Thread.sleep(1000);
+		
+		WebElement address = driver.findElement(By.cssSelector("#sView1\\:r1\\:0\\:address1\\:\\:content"));
+		address.sendKeys("bj road,cake nagar,Mydukur,Kadapa");
+		Thread.sleep(1000);
+		
+		WebElement city = driver.findElement(By.cssSelector("#sView1\\:r1\\:0\\:city\\:\\:content"));
+		city.sendKeys("Kadapa");
+		Thread.sleep(1000);
+		
+		WebElement state = driver.findElement(By.cssSelector("#sView1\\:r1\\:0\\:state\\:\\:content"));
+		state.sendKeys("Andhra pradesh");
+		Thread.sleep(1000);
+		
+		WebElement pinCode = driver.findElement(By.cssSelector("#sView1\\:r1\\:0\\:postalCode\\:\\:content"));
+		pinCode.sendKeys("516789");
+		
+		Thread.sleep(1000);
+		
+		WebElement register= driver.findElement(By.cssSelector("#sView1\\:r1\\:0\\:b1"));
+		register.click();   
+		
+	
+	}
+	@BeforeMethod
+	public void beforeMethod() {
+		
+		System.setProperty("webdriver.chrome.driver", "D:\\p5 installations\\chromedriver_win32\\\\\\\\chromedriver.exe");
+		driver= new ChromeDriver();
+	}
+	@AfterMethod
+	public void afterMethod() {
+		//driver.close();
+		driver=null;
+	}
+}
